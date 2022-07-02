609537a6c9340849c4ed7364ad8254e2b6116cb5
Kilian Brecht <kilianbrecht@gmx.de>
2022-07-02 12:42:28 +0200

    Score Logik für Predator zum laufen gebracht

--- RaeuberBeute/Simulation/Core/Simulation.cpp
+++ RaeuberBeute/Simulation/Core/Simulation.cpp
@@ -51,12 +51,14 @@ namespace sim {
 
 		Plant Grass("Grass");
 
+		map->setEntity(Fuchs, 0, 0);
+		map->setEntity(Fuchs, 9, 0);
 		map->setEntity(Fuchs, 1, 1);
 		map->setEntity(Fuchs, 2, 2);
 		std::cout << "Vision aus Map extrahiert: " << map->getEntity(2, 2)->getVision()->getRange() << std::endl;
 		std::cout << "Movement aus Map extrahiert: " << map->getEntity(2, 2)->getMovement()->getRange() << std::endl;
 		map->setEntity(Fuchs, 8, 8);
-		map->setEntity(Fuchs, 9, 9);
+		map->setEntity(Ente, 9, 9);
 		std::cout << "Vision aus Map extrahiert: " << map->getEntity(2, 3)->getVision()->getRange() << std::endl;
 		map->setEntity(Ente, 7, 7);
 		map->setEntity(Grass, 4, 4);
@@ -77,7 +79,6 @@ namespace sim {
 
 		int xMin, xMax;
 		int yMin, yMax;
-		int xBuf, yBuf;
 			  
 		if (role == predator) {
 			int vision = map->getEntity(xPos, yPos)->getVision()->getRange();
@@ -91,33 +92,48 @@ namespace sim {
 			yMin = yPos - vision < 0 ? 0 : yPos - vision;
 			yMax = map->getYSize() - 1 - yPos - vision < 0 ? map->getYSize() - 1 : yPos + vision;
 
-			//Unterscheiden von Faellen am Rande der Map
-			xBuf = xPos - xMin < vision ? xPos - xMin : xMax - xPos ? xMax - xPos : 0;
-			yBuf = yPos - yMin < vision ? yPos - yMin : yMax - yPos ? yMax - yPos : 0;
-
 			score.resize(xMax - xMin + 1, std::vector<int>(yMax - yMin + 1, 0));
 
-			std::cout << xBuf << ", " << yBuf << std::endl;
 			std::cout << xMin << ", " << xMax << ", " << yMin << ", " << yMax << std::endl;
 
 			//Felder im Umkreis durchgehen
 			for (int x = 0; x < xMax - xMin + 1; x++) {
 				for (int y = 0; y < yMax - yMin + 1; y++) {
 					std::cout << "Ueberpruefe bei x=" << x << ", y=" << y << "\n";
-					std::cout << "-> Map Pos:     x=" << x + xPos - xBuf << ", y=" << y + yPos - yBuf << "\n";
+					std::cout << "-> Map Pos:     x=" << x + xMin << ", y=" << y + yMin << "\n";
 
-					if (map->getEntity(x + xPos - xBuf - 1, y + yPos - yBuf - 1)->getRole() == prey)
+					if (map->getEntity(x + xMin, y + yMin)->getRole() == prey) {
+						//Score von dem Feld und den umliegenden Felder hochsetzen
 						score[x][y] += 4;
-					else if (map->getEntity(x + xPos - xBuf - 1, y + yPos - yBuf - 1)->getRole() == predator)
+						if (x > 0)
+							score[x - 1][y] += 2;
+						if (x > 0 && y > 0)
+							score[x - 1][y - 1] += 2;
+						if (y > 0)
+							score[x][y - 1] += 2;
+						if (y > 0 && x < xMax - xMin)
+							score[x + 1][y - 1] += 2;
+						if (x < xMax - xMin)
+							score[x + 1][y] += 2;
+						if (x < xMax - xMin && y < yMax - yMin)
+							score[x + 1][y + 1] += 2;
+						if (y < yMax - yMin)
+							score[x][y + 1] += 2;
+						if (x > 0 && y < yMax - yMin)
+							score[x - 1][y + 1] += 2;
+					}
+					else if (map->getEntity(x + xMin, y + yMin)->getRole() == predator) {
 						score[x][y] -= 10;
-
-					score[x][y] = x + vision - xMax == 0 && y + vision - yMax == 0 ? 0 : score[x][y];
+					}
+					
 					//neuen Bestscore setzen
 					highscore = score[x][y] > highscore ? score[x][y] : highscore;
 				}
 			}
-			//Aktuelles Feld auf neutral sezten, fuer z.B. den Fall, dass man komplett von Artgenossen umrundet ist
-			//score[xMin - xMax][yMin - yMax] = 0;
+
+			//Aktuelles Feld auf neutral setzen
+			score[xPos - xMin][yPos - yMin] = 0;
+
 
 			std::cout << "Highscore: " << highscore << std::endl;
 
